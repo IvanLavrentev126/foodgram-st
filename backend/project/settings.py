ec5e3494f60ab58caf89922735659d03568fab89
IvanLavrentev126 <154761282+IvanLavrentev126@users.noreply.github.com>
2025-07-20 22:56:33 +0300

    Update settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -124,7 +124,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'PAGE_SIZE': 6,
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
