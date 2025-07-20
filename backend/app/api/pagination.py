72155aeedde4ea2863d211384aed5cf69af8192b
IvanLavrentev126 <154761282+IvanLavrentev126@users.noreply.github.com>
2025-07-20 15:07:44 +0300

    Delete backend directory

--- backend/app/api/pagination.py
+++
@@ -1,8 +0,0 @@
-from ..constants import recipe_page_size
-from rest_framework.pagination import PageNumberPagination
-
-
-class RecipePagination(PageNumberPagination):
-    page_size = recipe_page_size
-    page_size_query_param = 'limit'
-    max_page_size = 100
--- backend/app/constants.py
+++
@@ -1,3 +0,0 @@
-recipe_page_size = 6
-MIN_TIME = 1
-MAX_TIME = 32000
